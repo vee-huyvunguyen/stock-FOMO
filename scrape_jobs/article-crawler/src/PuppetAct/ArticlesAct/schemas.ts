fdb5711d1cc445db0ce70faf0e72a4637cb019fe
huynv44 <huyk25sinh@gmail.com>
2025-01-03 23:07:38 +0700

    Updete: done ts schema for RawNewsPage

---
+++ scrape_jobs/article-crawler/src/PuppetAct/ArticlesAct/schemas.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon';
+
+interface RawNewsPage {
+    id: number
+    url: string
+    content_elements: string[]
+    author_element: string
+    post_datetime_element: string
+    category_element: string
+    content: string[]
+    author: string 
+    post_datetime: string 
+    category: string 
+    other_info: Record<string, string|number|boolean|Array<string|number|boolean>>
+    scrape_status: string
+    scraped_at: DateTime
+    inserted_at: DateTime
+}
+
+export {RawNewsPage}

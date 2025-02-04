718016f8f9300f28a0aa85c6cd768e1fd2df0803
huynv44 <huyk25sinh@gmail.com>
2025-02-04 22:00:47 +0700

    Update: Enhance RawArticlePage schema and init.sql to support multiple authors and article title (not just page title). Fixed: init.sql accept post_datetime as timestampz, while the scraped info is string

--- scrape_jobs/article-crawler/src/PuppetAct/ArticlesAct/schemas.ts
+++ scrape_jobs/article-crawler/src/PuppetAct/ArticlesAct/schemas.ts
@@ -4,12 +4,14 @@ import { ScrapeStatus } from '@/PuppetAct/ArticlesAct/ScrapeStatusHandler';
 type RawArticlePage = {
   url: string;
   pageTitle: string;
+  article_title_element?: string;
   content_elements?: string[];
-  author_element?: string;
+  author_element?: string[];
   post_datetime_element?: string;
   category_element?: string;
+  article_title?: string;
   content?: string[];
-  author?: string;
+  author?: string[];
   post_datetime?: string;
   category?: string;
   other_article_links?: string[];

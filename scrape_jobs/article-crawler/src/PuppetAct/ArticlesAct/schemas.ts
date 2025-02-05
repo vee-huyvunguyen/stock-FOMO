0560a26944709f7598db9914b8c93b11365448a6
huynv44 <huyk25sinh@gmail.com>
2025-02-05 10:41:23 +0700

    Update: CSSSelector.ts > Add example urls for each CNBC page type, make sure all selectors work with main/select/makit articles, Using regex to get elements with class that has random suffix. Feature: schemas.ts and init.sql > add article_updated_datetime, CSSSelector.ts > add selector for updated datetime

--- scrape_jobs/article-crawler/src/PuppetAct/ArticlesAct/schemas.ts
+++ scrape_jobs/article-crawler/src/PuppetAct/ArticlesAct/schemas.ts
@@ -6,13 +6,15 @@ type RawArticlePage = {
   pageTitle: string;
   article_title_element?: string;
   content_elements?: string[];
-  author_element?: string[];
-  post_datetime_element?: string;
+  author_elements?: string[];
+  article_published_datetime_element?: string;
+  article_updated_datetime_element?: string;
   category_element?: string;
   article_title?: string;
   content?: string[];
   author?: string[];
-  post_datetime?: string;
+  article_published_datetime?: string;
+  article_updated_datetime?: string;
   category?: string;
   other_article_links?: string[];
   other_links?: string[];
--- scrape_jobs/article-crawler/src/PuppetAct/CSSselectors.ts
+++ scrape_jobs/article-crawler/src/PuppetAct/CSSselectors.ts
@@ -19,32 +19,41 @@ type TypeBaseCSSSelector = {
 
 const CNBCActCSSselector: TypeBaseCSSSelector = {
   mainArticle: {
+    // Example: https://www.cnbc.com/2025/02/03/stock-market-today-live-updates.html
     checkLoadedPageElement: [
       '.branding-menu-logo',
       'href',
       'https://www.cnbc.com/world/',
     ],
     undesired: false,
-    contentElements: '.PageBuilder-article',
-    authorElements: '.Author-authorName',
+    // Title of the article is not always the .title of the html page
+    articleTitleElement: 'h1.LiveBlogHeader-headline',
+    contentElements: 'div.PageBuilder-article',
+    authorElements: 'a.Author-authorName', // sometimes there are multiple authors
+    // will try to get the datetime attribute, if not found, then get the text content
     postDatetimeElement:
       'time[data-testid="published-timestamp"][itemprop="datePublished"]',
+    updatedDatetimeElement:
+      'time[data-testid="lastpublished-timestamp"][itemprop="dateModified"]',
     categoryElement: '.ArticleHeader-eyebrow',
   },
   makeItArticle: {
+    // Example: https://www.cnbc.com/2025/01/28/tech-worker-saved-up-to-90-percent-of-his-pay-and-retired-with-3point5-million-dollars.html
     checkLoadedPageElement: [
       '.MakeItGlobalNav-styles-makeit-logo--sXqSs',
       'href',
       'https://www.cnbc.com/make-it/',
     ],
     undesired: false,
-    // TODO: check if the author elements can be selected with multiple authors
     contentElements: '[data-module="ArticleBody"]',
-    authorElements: '.Author-styles-makeit-authorName--_ANaL"',
-    postDatetimeElement: '.ArticleHeader-styles-makeit-time--hKeEh',
-    categoryElement: '.ArticleHeader-styles-makeit-eyebrow--Degp4',
+    authorElements: 'a[class^="Author-styles-makeit-authorName--"]',
+    postDatetimeElement: 'time[data-testid="published-timestamp"][itemprop="datePublished"]',
+    updatedDatetimeElement:
+      'time[data-testid="lastpublished-timestamp"][itemprop="dateModified"]',
+    categoryElement: 'a[class^="ArticleHeader-styles-makeit-eyebrow--"]', // get the href
   },
   selectArticle: {
+    // Example: https://www.cnbc.com/select/best-mortgage-lenders-first-time-homebuyers/
     checkLoadedPageElement: [
       '.logo-wrap > a:nth-child(1)',
       'href',
@@ -52,11 +61,14 @@ const CNBCActCSSselector: TypeBaseCSSSelector = {
     ],
     undesired: false,
     contentElements: '[data-module="ArticleBody"]',
-    authorElement: '.Author-styles-makeit-authorName--_ANaL"',
-    postDatetimeElement: '.ArticleHeader-styles-makeit-time--hKeEh',
-    categoryElement: '.ArticleHeader-styles-makeit-eyebrow--Degp4',
+    authorElement: 'a[class^="Author-styles-select-authorName--"]',
+    // Can only get as text, doesn't have datetime attribute
+    // sometimes "Updated 5 minutes ago" -> have to infer later
+    postDatetimeElement: '[class^="ArticleHeader-styles-select-time--"]',
+    categoryElement: '[class^="ArticleHeader-styles-select-eyebrow--"]', // get the href
   },
   podcastArticle: {
+    // Example: https://www.cnbc.com/podcasts/the-takeout/
     checkLoadedPageElement: [
       '.PageHeaderWithTuneInText-title',
       'innerHTML',
@@ -65,6 +77,7 @@ const CNBCActCSSselector: TypeBaseCSSSelector = {
     undesired: true,
   },
   proArticle: {
+    // Example: https://www.cnbc.com/2025/02/04/mondays-turnaround-showed-little-guy-continues-to-drive-bull-market.html
     checkLoadedPageElement: [
       '.ProPill-proPillLink',
       'href',
